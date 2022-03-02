3389db2905cbf16456e4a2b9d9d1c8986267c34f
Nina Schuchman <nschuchman@hubspot.com>
2022-03-02 09:43:26 -0500

    updates

--- source_data/citi.sh
+++ source_data/citi.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# mv ~/Downloads/Statement*.CSV data/citi_stg.csv
+mv ~/Downloads/Statement*.CSV data/citi_stg.csv
 mv ~/Downloads/Since*.CSV data/citi_stg.csv
 sed -i.bak "1d" data/citi_stg.csv
 cat data/headers/citi_headers.txt data/citi_stg.csv > data/citi_transactions.csv
