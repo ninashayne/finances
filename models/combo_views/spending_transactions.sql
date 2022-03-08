f8c649ae9366bf3dd3a276deb527d6d0edc0611b
Nina Schuchman <nschuchman@hubspot.com>
2022-03-08 09:19:43 -0500

    add flag for spending transactions

--- models/combo_views/spending_transactions.sql
+++ models/combo_views/spending_transactions.sql
@@ -1,6 +1,7 @@
 select date_trunc('month', transaction_date::date)::date as transaction_month
 , *
 , max(update_dt) over (partition by source_name) as source_latest_update_dt
+, NOT(coalesce(category_l1,'null') in ('trasnfer', 'transfer', 'credit', 'betterment', 'income', 'payment', 'refund', 'reimbursement', 'water bill')) as spending_transaction_flag
 from {{ ref('stg_transactions') }}
 {# where not coalesce(is_reimbursed_by_house, False)
 and not coalesce(is_cancelled, false)
