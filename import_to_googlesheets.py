4a779abbed1bf15c4ebe18baa34e0b6bd7d945d7
Nina Fink <ninafink@Ninas-MacBook-Pro.local>
2022-04-28 11:13:57 -0400

    new files

--- import_to_googlesheets.py
+++ import_to_googlesheets.py
@@ -2,14 +2,17 @@
 
 import pygsheets
 import pandas as pd
-import psycopg2
+from sqlalchemy import create_engine
+
 #authorization
 gc = pygsheets.authorize(service_file='/Users/ninafink/Desktop/nina_personal/finances/my_json_file.json')
 
-# # connect to database
-conn = psycopg2.connect("dbname='restored-db2' user='postgres' host='localhost' password='test'")
 
-# print(conn)
+# # connect to database
+connection_string = 'postgresql://postgres:test@localhost:5432/restored-db2'
+conn = create_engine(connection_string)
+
+
 
 # open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
 sh = gc.open('dan_and_nina_transactions')
@@ -20,7 +23,7 @@ wks = sh.worksheet('title','Transactions')
 
 # create dataframe from query
 sql = "select * from fink_finances.spending_transactions"
-df = pd.read_sql(sql, conn)
+df = pd.read_sql_query(sql, conn)
 
 
 wks.set_dataframe(df,(1,1))
